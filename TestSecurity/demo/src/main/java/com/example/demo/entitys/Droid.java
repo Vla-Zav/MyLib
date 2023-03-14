9bd705d9b85015626cee57dfdf499c47b4934a25
Vla-Zav <106822298+Vla-Zav@users.noreply.github.com>
2023-03-14 17:30:05 +0300

    Non

--- TestSecurity/demo/src/main/java/com/example/demo/entitys/Droid.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entitys;
-
-import lombok.Data;
-
-@Data
-public class Droid {
-    private String id, description;
-}
--- TestSecurity/demo/src/main/java/com/example/demo/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entitys.Coffee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CoffeeRepository extends CrudRepository<Coffee, String> {
-}
