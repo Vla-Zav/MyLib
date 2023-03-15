907d25a8c516070c67770c17213ad497b2b5fd56
Vla-Zav <106822298+Vla-Zav@users.noreply.github.com>
2023-03-15 19:06:24 +0300

    Update PersonController.java

--- Tests/ForJunitLearn/src/main/java/com/example/forjunitlearn/controllers/PersonController.java
+++ Tests/ForJunitLearn/src/main/java/com/example/forjunitlearn/controllers/PersonController.java
@@ -4,44 +4,61 @@ import com.example.forjunitlearn.entitys.Person;
 import com.example.forjunitlearn.repositorys.PersonRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/persons")
 @AllArgsConstructor
 public class PersonController {
     private PersonRepository personRepository;
+
     @GetMapping
-    public List<Person> listAllPersons(){
-        return personRepository.findAll();
+    public ResponseEntity<List<Person>> listAllPersons() {
+        List<Person> persons = personRepository.findAll();
+        return ResponseEntity.ok().body(persons);
+
     }
 
     @GetMapping("/{personId}")
-    public Person getPersonById(@PathVariable Long personId){
-        return personRepository.findById(personId).orElseThrow(EntityNotFoundException::new);
+    public ResponseEntity<Person> getPerson(@PathVariable Long personId)
+            throws EntityNotFoundException {
+
+        Optional<Person> person = personRepository.findById(personId);
+        if (!person.isPresent())
+            throw new EntityNotFoundException("id-" + personId);
+        return ResponseEntity.ok().body(person.get());
+
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Person> createPerson(@RequestBody @Valid Person person){
-        Person createdPerson = personRepository.save(person);
-        return ResponseEntity.status(201).body(createdPerson);
+    @PostMapping
+    public ResponseEntity<Person> createPerson(@RequestBody @Valid Person person) {
+        Person p = personRepository.save(person);
+        return ResponseEntity.status(201).body(p);
     }
 
     @PutMapping("/{personId}")
-    public Person updatePerson(@RequestBody @Valid Person person , @PathVariable Long personId){
-        Person oldPerson = personRepository.findById(personId).orElseThrow(EntityNotFoundException::new);
-        oldPerson.setName(person.getName());
-        return personRepository.save(oldPerson);
+    public ResponseEntity<Person> updatePerson(@RequestBody @Valid Person person,
+                                               @PathVariable Long personId) throws EntityNotFoundException {
+        Optional<Person> p = personRepository.findById(personId);
+        if (!p.isPresent())
+            throw new EntityNotFoundException("id-" + personId);
+        return ResponseEntity.ok().body(personRepository.save(person));
     }
 
     @DeleteMapping("/{personId}")
-    public ResponseEntity<Person> deletePerson(@PathVariable Long personId){
-        Person delPerson = personRepository.findById(personId).orElseThrow(EntityNotFoundException::new);
+    public ResponseEntity<Person> deletePerson(@PathVariable Long personId)
+            throws EntityNotFoundException {
+        Optional<Person> person = personRepository.findById(personId);
+        if (!person.isPresent())
+            throw new EntityNotFoundException("id-" + personId);
         personRepository.deleteById(personId);
-        return ResponseEntity.ok().body(delPerson);
+        return ResponseEntity.ok().body(person.get());
     }
 }
