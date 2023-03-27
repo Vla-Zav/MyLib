722fabc1b710c10c0b26213301b306c457c9d427
Vla-Zav <106822298+Vla-Zav@users.noreply.github.com>
2023-03-27 20:16:56 +0300

    j

--- TestHotNaChtoto/src/test/java/com/example/testhotnachtoto/TestHotNaChtotoApplicationTests.java
+++ TestHotNaChtoto/src/test/java/com/example/testhotnachtoto/TestHotNaChtotoApplicationTests.java
@@ -6,13 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
-
 class TestHotNaChtotoApplicationTests {
 
     @Autowired
