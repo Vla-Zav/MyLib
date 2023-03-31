63e855c0c9c8f6903570a36548166a59033dbd61
ChaoticBD <Chaoticbd@me.com>
2023-03-31 20:49:03 +0300

    try this

--- LiquibaseAndTestContainers/src/test/java/com/example/liquibaseandtestcontainers/LiquibaseAndTestContainersApplicationTests.java
+++ LiquibaseAndTestContainers/src/test/java/com/example/liquibaseandtestcontainers/LiquibaseAndTestContainersApplicationTests.java
@@ -1,32 +1,36 @@
 package com.example.liquibaseandtestcontainers;
 
+import com.example.liquibaseandtestcontainers.controllers.UserController;
 import com.example.liquibaseandtestcontainers.entitys.User;
 import com.example.liquibaseandtestcontainers.repa.UsersRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.List;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 @Testcontainers
 class LiquibaseAndTestContainersApplicationTests {
 
-	@Autowired
-	private UsersRepository usersRepository;
-	@Test
-	void findUserById() {
-		var user = usersRepository.findById(1L);
-		Assertions.assertThat(user.get().getUsername()).isEqualTo("admin");
-	}
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private UserController userController;
+
+    @Test
+    void findUserById() {
+        var user = usersRepository.findById(1L);
+        Assertions.assertThat(user.get().getUsername()).isEqualTo("admin");
+    }
+
+    @Test
+    void findUser() {
+        List<User> users = userController.getUsers().getBody();
+        Assertions.assertThat(users).isNotNull();
+        Assertions.assertThat(users.size()).isEqualTo(2);
+    }
 }
