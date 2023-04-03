6d3df66a0031f970adef56ef3b88a5806d0df3d9
Vla-Zav <106822298+Vla-Zav@users.noreply.github.com>
2023-04-03 22:16:48 +0300

    IntegrationTests
    Надеюсь, что я правильно понял

--- LiquibaseAndTestContainers/src/main/java/com/example/liquibaseandtestcontainers/controllers/UserController.java
+++ LiquibaseAndTestContainers/src/main/java/com/example/liquibaseandtestcontainers/controllers/UserController.java
@@ -2,13 +2,13 @@ package com.example.liquibaseandtestcontainers.controllers;
 
 import com.example.liquibaseandtestcontainers.entitys.User;
 import com.example.liquibaseandtestcontainers.repa.UsersRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,5 +27,30 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable Long id){
         User user = usersRepository.findById(id).get();
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody @Valid User newUser){
+        usersRepository.save(newUser);
+        return ResponseEntity.status(201).body(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> putUser(@PathVariable Long id, @RequestBody @Valid User editUser) throws EntityNotFoundException {
+        if(usersRepository.findById(id).isPresent()) {
+            editUser.setId(id);
+            usersRepository.save(editUser);
+            return ResponseEntity.ok().body(editUser);
+        }
+        else return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id){
+        if(usersRepository.findById(id).isPresent()) {
+            usersRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        else return ResponseEntity.notFound().build();
     }
 }
--- LiquibaseAndTestContainers/src/test/java/com/example/liquibaseandtestcontainers/LiquibaseAndTestContainersApplicationTests.java
+++ LiquibaseAndTestContainers/src/test/java/com/example/liquibaseandtestcontainers/LiquibaseAndTestContainersApplicationTests.java
@@ -4,13 +4,15 @@ import com.example.liquibaseandtestcontainers.controllers.UserController;
 import com.example.liquibaseandtestcontainers.entitys.User;
 import com.example.liquibaseandtestcontainers.repa.UsersRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -23,20 +25,42 @@ class LiquibaseAndTestContainersApplicationTests {
     private UserController userController;
 
     @Test
-    void findUserById() {
-        var user = usersRepository.findById(1L);
-        Assertions.assertThat(user.get().getUsername()).isEqualTo("admin");
+    void testUserCreate(){
+        User newUser = new User();
+        newUser.setUsername("Vlad");
+        newUser.setPassword("pass");
+        newUser.setEmail("test@mail.ru");
+        Assertions.assertThat(userController.createUser(newUser).getStatusCode().value()).isEqualTo(201);
+        Assertions.assertThat(usersRepository.count()).isEqualTo(3);
     }
-
     @Test
-    void findUser() {
+    void findUsersTest() {
         List<User> users = userController.getUsers().getBody();
         Assertions.assertThat(users).isNotNull();
         Assertions.assertThat(users.size()).isEqualTo(2);
     }
 
     @Test
-    void controllerTest(){
+    void testGetUserById(){
+        User user = userController.getUserById(1L).getBody();
+        Assertions.assertThat(user.getUsername()).isEqualTo("admin");
+    }
 
+    @Test
+    void testPutUser(){
+        Assertions.assertThat(usersRepository.findById(1L).get().getUsername()).isEqualTo("admin");
+        User user = new User();
+        user.setUsername("newAdm");
+        user.setEmail("newadm@mail.ru");
+        user.setPassword("newPass");
+        Assertions.assertThat(userController.putUser(1L, user).getStatusCode().value()).isEqualTo(200);
+        User editUser = usersRepository.findById(1L).get();
+        Assertions.assertThat(editUser.getUsername()).isEqualTo("newAdm");
+    }
+
+    @Test
+    void testDeleteUser(){
+        userController.deleteUser(1L);
+        Assertions.assertThat(usersRepository.count()).isEqualTo(1);
     }
 }
